9addb1bd8b80046fed08cadb75468c03c4d3438d
DGrazz <mariogalo2008@gmail.com>
2024-06-23 18:06:46 +0200

    Update login page design and components

---
+++ nuxt/components/Section-Info&Image.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="h-screen w-full flex justify-center items-center" :class="{ 'flex-row-reverse': infoRight }">
+        <section class="w-[40%] h-[80%] bg-background2" :class="{'rounded-l-3xl': !infoRight, 'rounded-r-3xl': infoRight}">
+            <div class="flex flex-col justify-center items-center h-full gap-12">
+                <slot />
+            </div>
+        </section>
+
+        <section class="w-[60%] h-[80%] overflow-hidden" :class="{'rounded-r-3xl': !infoRight, 'rounded-l-3xl': infoRight}">
+            <img class="w-full h-full object-cover" :src="image" alt="">
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        image: {
+            type: String,
+            required: true
+        },
+
+        infoRight: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    data() {
+        return {
+        };
+    },
+    
+}
+</script>
---
+++ nuxt/pages/register.vue
@@ -0,0 +1,18 @@
+<template>
+    <Section-Info&Image image="/green-bg.png" infoRight>
+        <header class="flex flex-col justify-center items-center gap-3">
+            <Logo class="size-16 mb-4" />
+            <h1 class="text-4xl font-bold">Bienvenido!</h1>
+            <p class="w-2/3 text-center text-pretty">Únete y lleva una gestion sencilla y
+                comoda de todas tus sneakers</p>
+        </header>
+
+        
+    </Section-Info&Image>
+</template>
+
+<script>
+export default {
+
+}
+</script>
