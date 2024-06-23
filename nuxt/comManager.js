c53146310c478fba335ede1383c173cead21123f
Pedro Garcia <ttittis2004@gmail.com>
2024-06-23 23:43:39 +0200

    url modificada

--- nuxt/comManager.js
+++ nuxt/comManager.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const url = 'http://localhost:8000';
+
 async function registerGoogle() {
-    window.location.href = 'http://localhost:8000/auth/google';
+    window.location.href = `${url}/auth/google`;
 }
 
 async function getUser(token) {
 
-    const res = await axios.get('http://localhost:8000/api/user', {
+    const res = await axios.get(`${url}/api/user`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
