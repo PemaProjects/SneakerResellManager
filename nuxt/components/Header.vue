b04cbbbef7e954d59d72320c09425f7e35393e3e
DGrazz <mariogalo2008@gmail.com>
2024-06-24 12:10:44 +0200

    Update header and index page styles for responsiveness

--- nuxt/components/Header.vue
+++ nuxt/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex justify-between items-center">
-        <div class="flex items-center gap-4 py-4">
-            <Logo class="size-16" />
-            <h1 class="text-2xl font-bold">SNKRSMAN</h1>
+        <div class="flex items-center gap-2 md:gap-4 py-4">
+            <Logo class="size-10 md:size-16" />
+            <h1 class="md:text-2xl font-bold">SNKRSMAN</h1>
         </div>
 
-        <NuxtLink to="/login" class="inline-block border border-white font-bold py-2 px-5 rounded-md hover:bg-white hover:text-black transition duration-200">Log In</NuxtLink>
+        <NuxtLink to="/login" class="text-xs md:text-base inline-block md:border border-white font-bold py-2 px-5 rounded-md hover:bg-white hover:text-black transition duration-200">Log In</NuxtLink>
     </div>
 </template>
 
