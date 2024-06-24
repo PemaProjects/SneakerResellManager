868dc9fc47aa0a93f7a1a6b3377350e957239f3c
Pedro Garcia <ttittis2004@gmail.com>
2024-06-24 17:48:21 +0200

    Adidas webscrapping

--- WebScrapping/adidas/script.py
+++ WebScrapping/adidas/script.py
@@ -1,26 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 import re
 import mysql.connector
-
-
-chrome_options = Options()
-chrome_options.add_argument("--headless") 
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-
-selenium_service = ChromeService('/usr/bin/chromedriver')
-driver = webdriver.Remote(
-    command_executor='http://selenium:4444/wd/hub',
-    options=chrome_options
-)
+import requests
+import json
 
 conn = mysql.connector.connect(
     host='db',
@@ -31,23 +14,50 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
-page = 1
 total = 0
 
-url = f"https://www.adidas.es/api/plp/content-engine?&query=zapatillas-hombre&start=0"
-driver.get(url)
+hdr = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers'
+}
 
-time.sleep(5)
+def scrap_adidas(url):
+    total = 0
+    while True:
+        html_page = requests.get(url + f"?start={total}", headers=hdr, timeout=15)
+        soup = BeautifulSoup(html_page.content, 'html.parser')
 
-page_content = driver.page_source
+        paginationNext = soup.find('a', attrs={'data-auto-id': 'plp-pagination-next'})
 
-soup = BeautifulSoup(page_content, 'html.parser')
+        sneakers = soup.find_all('div', class_='grid-item')
 
-print(soup)
+        for sneaker in sneakers:
+            total += 1
+
+            name = sneaker.find('p', class_='glass-product-card__title')
+            img = sneaker.find('img', attrs={'data-auto-id': 'product-card-image'})
+            
+            cursor.execute("SELECT name FROM sneakers WHERE name = %s", (name.text,))
+            result = cursor.fetchone()
+            
+            if result is None:
+                sql = "INSERT INTO sneakers (name, img) VALUES (%s, %s)"
+                values = (name.text, img.get('src'))
+                cursor.execute(sql, values)
+                conn.commit()
+                print(f"Sneaker introducido correctamente")
+            else:
+                print(f"El sneaker {name.text} ya está en la base de datos")
+        if not paginationNext:
+            break
+
+scrap_adidas("https://www.adidas.com/us/men-shoes")
+scrap_adidas("https://www.adidas.com/us/women-shoes")
 
 cursor.close()
 conn.close()
-
-driver.quit()
-
-print(f"\nTotal de elementos encontrados: {total}")
