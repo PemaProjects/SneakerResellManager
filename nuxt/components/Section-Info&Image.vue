83b62170c5d3991bd89131cb9c693b77a1719519
DGrazz <mariogalo2008@gmail.com>
2024-06-25 21:41:24 +0200

    refactor: Update styles and responsiveness of header and index page

--- nuxt/components/Section-Info&Image.vue
+++ nuxt/components/Section-Info&Image.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="h-screen w-full flex justify-center items-center" :class="{ 'flex-row-reverse': infoRight }">
-        <section class="w-[40%] h-[80%] bg-background2" :class="{'rounded-l-3xl': !infoRight, 'rounded-r-3xl': infoRight}">
-            <div class="flex flex-col justify-center items-center h-full gap-8">
+    <div class="h-screen w-full flex flex-col-reverse lg:flex-row justify-center items-center" :class="{ 'lg:flex-row-reverse': infoRight }">
+        <section class="w-screen h-[85vh] md:w-[80%] md:h-[60%] lg:w-[40%] lg:h-[80%] bg-background2 md:rounded-b-3xl lg:rounded-none" :class="{'lg:rounded-l-3xl': !infoRight, 'lg:rounded-r-3xl': infoRight}">
+            <div class="flex flex-col justify-center items-center h-full gap-8 md:gap-14 lg:gap-8">
                 <slot />
             </div>
         </section>
 
-        <section class="w-[60%] h-[80%] overflow-hidden" :class="{'rounded-r-3xl': !infoRight, 'rounded-l-3xl': infoRight}">
+        <section class="w-screen h-[15vh] md:w-[80%] md:h-[20%] lg:w-[60%] lg:h-[80%] overflow-hidden md:rounded-t-3xl lg:rounded-none" :class="{'lg:rounded-r-3xl': !infoRight, 'lg:rounded-l-3xl': infoRight}">
             <img class="w-full h-full object-cover" :src="image" alt="">
         </section>
     </div>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -4,9 +4,7 @@
         <div class="space-y-4 md:space-y-6 lg:space-y-8">
             <h1 class="uppercase font-bold text-2xl text-center text-balance md:text-4xl lg:text-left lg:text-[42px] 2xl:text-6xl">Simplifica la reventa de <span
                     class="text-primary">sneakers</span> y maximiza tus ganancias</h1>
-            <p class="text-grayText font-medium text-pretty text-center md:w-3/4 md:mx-auto lg:mx-0 lg:text-left 2xl:w-3/4">Lleva un control preciso de tu
-                inventario, gestiona tus ventas y optimiza cada transacción. Ideal para revendedores de sneakers que
-                buscan eficiencia y crecimiento.</p>
+            <p class="text-grayText font-medium text-pretty text-center md:w-3/4 md:mx-auto lg:mx-0 lg:text-left 2xl:w-3/4">Nuestra plataforma ayuda a revendedores de sneakers a gestionar inventario y ventas eficientemente.</p>
             <div class="text-xs flex justify-between md:justify-center md:gap-10 md:text-sm lg:text-base lg:block lg:space-x-12 [&>button:hover]:-translate-y-2 [&>button]:transition [&>button]:duration-200">
                 <button class="  bg-gradient-to-r from-primary to-[#B6D497] text-black font-bold px-4 py-3 rounded-md">Ir
                     al dashboard</button>
