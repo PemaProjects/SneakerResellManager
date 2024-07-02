558fd8944e9ee090ec3ea1ebf3b63286014747d3
DGrazz <mariogalo2008@gmail.com>
2024-07-02 15:28:30 +0200

    refactor: Update sidebar in inventory page and add loaders to login and register buttons

--- nuxt/components/SideBar.vue
+++ nuxt/components/SideBar.vue
@@ -1,57 +1,55 @@
 <template>
-    <div 
-        :class="active ? 'w-80' : 'w-32'"	    
-        class="h-screen fixed top-0 left-0 flex flex-col gap-12 py-6 px-12 border transition duration-500" 
-        @mouseover="active = true"
-        @mouseleave="active = false">
-        <div
-            :class="active ? '[&>a]:px-4' : ''"
-            class="flex flex-col justify-center items-start gap-6 [&>a]:text-white [&>a]:py-3 [&>a]:w-full [&>a]:transition [&>a]:rounded-md [&>a]:duration-200 [&>a:hover]:bg-selected [&>a:hover]:text-primary">
-            <div class="flex gap-4 items-center">
-                <Logo class="size-10" />
-                <h1 
-                :class="active ? 'block' : 'hidden'"
-                class="text-3xl font-bold">SNKRSMAN</h1>
+    <section>
+        <nav :style="{ width: active ? '20rem' : '8rem' }"
+            class="h-screen fixed top-0 left-0 flex flex-col gap-12 py-10 px-12 transition-all duration-500 ease-in-out border"
+            @mouseover="active = true" @mouseleave="active = false">
+            <div class="h-full flex flex-col justify-between items-center">
+                <div :class="active ? '[&>a]:px-4 [&>a]:w-full' : ''"
+                    class="flex flex-col justify-center items-start gap-6 [&>a]:text-white [&>a]:py-3 [&>a]:transition [&>a]:rounded-md [&>a]:duration-200 [&>a:hover]:bg-selected [&>a:hover]:text-primary">
+                    <div class="flex gap-4 items-center">
+                        <Logo class="size-10" />
+                        <h1 :class="active ? 'block' : 'hidden'" class="text-3xl font-bold">SNKRSMAN</h1>
+                    </div>
+                    <NuxtLink to="/dashboard" exact-active-class="text-primary" class="flex items-center gap-3">
+                        <IconsDashboard class="size-7" />
+                        <span :class="active ? 'block' : 'hidden'">Dashboard</span>
+                    </NuxtLink>
+
+                    <NuxtLink to="/inventario" exact-active-class="text-primary" class="flex items-center gap-3">
+                        <IconsList class="size-7" />
+                        <span :class="active ? 'block' : 'hidden'">Inventario</span>
+                    </NuxtLink>
+
+                    <NuxtLink to="/estadisticas" exact-active-class="text-primary" class="flex items-center gap-3">
+                        <IconsStatistics class="size-7" />
+                        <span :class="active ? 'block' : 'hidden'">Estadisticas</span>
+                    </NuxtLink>
+
+                    <NuxtLink to="/tickets" exact-active-class="text-primary" class="flex items-center gap-3">
+                        <IconsTicket class="size-7" />
+                        <span :class="active ? 'block' : 'hidden'">Tickets</span>
+                    </NuxtLink>
+
+                    <NuxtLink to="/perfil" exact-active-class="text-primary" class="flex items-center gap-3">
+                        <IconsProfile class="size-7" />
+                        <span :class="active ? 'block' : 'hidden'">Perfil</span>
+                    </NuxtLink>
+
+                    <NuxtLink to="/settings" exact-active-class="text-primary" class="flex items-center gap-3">
+                        <IconsSetting class="size-7" />
+                        <span :class="active ? 'block' : 'hidden'">Ajustes</span>
+                    </NuxtLink>
+                </div>
+
+                <button :class="active ? 'px-4 w-full' : ''"
+                    class="flex  items-center gap-3 py-3 transition duration-200 hover:bg-[#BB6C6C]/10 hover:text-[#BB6C6C] rounded-md">
+                    <IconsExit class="size-7" />
+                    <span :class="active ? 'block' : 'hidden'">Logout</span>
+                </button>
             </div>
-            <NuxtLink to="/dashboard" exact-active-class="text-primary" class="flex items-center gap-3">
-                <IconsDashboard class="size-7" />
-                <span :class="active ? 'block' : 'hidden'">Dashboard</span>
-            </NuxtLink>
+        </nav>
 
-            <NuxtLink to="/inventario" exact-active-class="text-primary" class="flex items-center gap-3">
-                <IconsList class="size-7" />
-                <span :class="active ? 'block' : 'hidden'">Inventario</span>
-            </NuxtLink>
-
-            <NuxtLink to="/estadisticas" exact-active-class="text-primary" class="flex items-center gap-3">
-                <IconsStatistics class="size-7" />
-                <span :class="active ? 'block' : 'hidden'">Estadisticas</span>
-            </NuxtLink>
-
-            <NuxtLink to="/tickets" exact-active-class="text-primary" class="flex items-center gap-3">
-                <IconsTicket class="size-7" />
-                <span :class="active ? 'block' : 'hidden'">Tickets</span>
-            </NuxtLink>
-
-            <NuxtLink to="/perfil" exact-active-class="text-primary" class="flex items-center gap-3">
-                <IconsProfile class="size-7" />
-                <span :class="active ? 'block' : 'hidden'">Perfil</span>
-            </NuxtLink>
-
-            <NuxtLink to="/settings" exact-active-class="text-primary" class="flex items-center gap-3">
-                <IconsSetting class="size-7" />
-                <span :class="active ? 'block' : 'hidden'">Ajustes</span>
-            </NuxtLink>
-
-            <button
-            :class="active ? 'px-4' : ''"
-                class="w-full flex items-center gap-3 py-3 transition duration-200 hover:bg-[#BB6C6C]/10 hover:text-[#BB6C6C] rounded-md">
-                <IconsExit class="size-7" />
-                <span :class="active ? 'block' : 'hidden'">Logout</span>
-            </button>
-        </div>
-
-    </div>
+    </section>
 </template>
 
 <script>
