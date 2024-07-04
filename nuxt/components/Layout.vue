6aad4b22ef314f0ce158a1ed186b179783d5e386
DGrazz <mariogalo2008@gmail.com>
2024-07-04 15:12:25 +0200

    refactor: Update sidebar in Layout.vue to use full width

--- nuxt/components/Layout.vue
+++ nuxt/components/Layout.vue
@@ -5,7 +5,7 @@
         <main class="w-full h-screen p-6 pl-0">
             <div class="bg-background2 w-full h-full rounded-2xl py-4 px-6">
                 <Header class="text-4xl font-semibold pt-4" :title="title" />
-                <div class="max-h-full bg-background rounded-2xl py-6 px-6">
+                <div class="w-full bg-background rounded-2xl py-6 px-6">
                     <slot></slot>
                 </div>
 
