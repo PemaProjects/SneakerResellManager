3140873497cd46035740a2689c05a28176f4084a
DGrazz <mariogalo2008@gmail.com>
2024-07-04 15:03:00 +0200

    refactor: Update sidebar styling and header layout

--- nuxt/components/Layout.vue
+++ nuxt/components/Layout.vue
@@ -1,11 +1,11 @@
 <template>
-    <section class="flex overflow-hidden">
+    <section class="flex ">
         <SideBar class=""/>
 
         <main class="w-full h-screen p-6 pl-0">
             <div class="bg-background2 w-full h-full rounded-2xl py-4 px-6">
-                <Header class="text-4xl font-semibold" :title="title" />
-                <div class="bg-background rounded-2xl py-6 px-6">
+                <Header class="text-4xl font-semibold pt-4" :title="title" />
+                <div class="max-h-full bg-background rounded-2xl py-6 px-6">
                     <slot></slot>
                 </div>
 
