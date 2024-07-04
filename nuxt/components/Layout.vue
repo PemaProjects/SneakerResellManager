21e3df3a5f6ebb335a3d317b87e1e23166ef0202
DGrazz <mariogalo2008@gmail.com>
2024-07-04 17:04:32 +0200

    refactor: Update sidebar styling and layout in Layout.vue

--- nuxt/components/Layout.vue
+++ nuxt/components/Layout.vue
@@ -1,14 +1,13 @@
 <template>
-    <section class="flex ">
+    <section class="flex">
         <SideBar class=""/>
 
-        <main class="w-full h-screen p-6 pl-0">
-            <div class="bg-background2 w-full h-full rounded-2xl py-4 px-6">
+        <main class="w-full h-screen p-6 pl-0 flex flex-col">
+            <div class="bg-background2 w-full h-full rounded-2xl py-4 pb-6 px-6 flex flex-col">
                 <Header class="text-4xl font-semibold pt-4" :title="title" />
-                <div class="w-full bg-background rounded-2xl py-6 px-6">
+                <div class="w-full flex-1 bg-background rounded-2xl py-6 px-6 mt-4 overflow-y-auto">
                     <slot></slot>
                 </div>
-
             </div>
         </main>
     </section>
@@ -19,6 +18,5 @@
         props: {
             title: String
         }
-
     }
 </script>
