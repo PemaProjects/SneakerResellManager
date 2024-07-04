73365a8e139ea0b147eb10a572ec06d79eac6d7c
DGrazz <mariogalo2008@gmail.com>
2024-07-04 14:44:42 +0200

    refactor: Update sidebar styling and add loaders to login and register buttons

--- nuxt/components/Layout.vue
+++ nuxt/components/Layout.vue
@@ -1,11 +1,13 @@
 <template>
-    <section class="flex">
+    <section class="flex overflow-hidden">
         <SideBar class=""/>
 
         <main class="w-full h-screen p-6 pl-0">
             <div class="bg-background2 w-full h-full rounded-2xl py-4 px-6">
                 <Header class="text-4xl font-semibold" :title="title" />
-                <slot></slot>
+                <div class="bg-background rounded-2xl py-6 px-6">
+                    <slot></slot>
+                </div>
 
             </div>
         </main>
