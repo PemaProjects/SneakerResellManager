3ae2e677c6d5d704e44b223699e7724aaa646970
DGrazz <mariogalo2008@gmail.com>
2024-07-09 12:50:11 +0200

    refactor: Update AddSneaker component to use smaller font size for title

--- nuxt/components/AddSneaker.vue
+++ nuxt/components/AddSneaker.vue
@@ -1,7 +1,7 @@
 <template>
     <div @click="closeModal" class="absolute top-0 left-0 w-screen h-screen backdrop-blur-sm flex justify-center items-center">
         <div class="bg-background2 px-20 py-10 rounded-lg space-y-2">
-            <h3 class="text-lg">Añade tu sneaker</h3>
+            <h3 class="text-sm text-grayText font-semibold">Añade una nueva sneaker</h3>
             <div class="flex bg-background2 rounded-md w-full items-center border text-grayText">
                 <IconsSearch class="ml-3 size-7" />
                 <input class="text-xl font-semibold rounded-md w-96 px-3 py-2 bg-transparent outline-none" v-model="buscar"
